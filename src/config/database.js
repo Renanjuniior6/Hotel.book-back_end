cf3b82e14ef2d0435316bac270c719e57ad3f09f
Renanjuniior6 <renanjrr679@gmail.com>
2024-02-02 18:33:34 -0300

    Criação da migration e alterado o node_modules

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 module.exports = {
   dialect: "postgres",
-  url: "postgresql://postgres:fDca5g4aCd*b1eA13-D-443cb33fbbdc@roundhouse.proxy.rlwy.net:39797/railway",
+  host: "localhost",
+  username: "postgres",
+  password: "frost",
+  database: "Hotel.Book",
   define: {
     timestamps: true,
     underscored: true,
