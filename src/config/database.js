5eea7d9b467aff3a0479d9ef1a7248e6b2b1d79f
Renanjuniior6 <renanjrr679@gmail.com>
2024-02-03 19:40:05 -0300

    projeto refeito com node atualizado

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+export default {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "frost",
+  database: "Hotel.Book",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
