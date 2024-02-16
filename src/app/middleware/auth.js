6bb09802b53c584bc703c1f69c07007c38ad50e4
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-02-16 19:38:22 -0400

    sem alterações

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,9 +1,25 @@
 import Jwt from "jsonwebtoken"
+import authConfig from "../../config/auth"
 
 export default (request, response, next) => {
   const authToken = request.headers.authorization
 
   if (!authToken) {
     return response.status(401).JSON({ id: 'not Token provided' })
+  }
+
+  const token = authToken.split(' ')[1]
+
+  try{
+    Jwt.verify(token, authConfig.secret, function(err, decoded){
+      if(err){
+        throw new Error()
+      }
+
+      request.userId = decoded.id
+      return next()
+    })
+  }catch(err){
+    return response.status(401).json({error: 'Token is invalid'})
   }
 }
